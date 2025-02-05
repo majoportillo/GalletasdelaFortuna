1c3123a1f89e0c929cf930cf7e4d35bca108ad69
majitomoomin <mariajoseportill2@gmail.com>
2025-02-04 23:00:34 -0600

    Mostrar mensaje
    Error al dar click en el boton

--- src/Ventana.java
+++ src/Ventana.java
@@ -1,7 +1,12 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Ventana extends JFrame {
     private JButton boton;
+    private JLabel mensaje;
+    private MensajesFortuna mostrarmensaje;
 
 
     public Ventana(){
@@ -10,10 +15,23 @@ public class Ventana extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
+        mostrarmensaje = new MensajesFortuna();
+        mensaje = new JLabel("Conoce tu fortuna", SwingConstants.CENTER);
+        mensaje.setBounds(50, 50, 400, 30);
+        add(mensaje);
+
         boton = new JButton("Descubre tu fortuna");
         boton.setBounds(200, 200, 100, 50);
 
         add(boton);
+
+        boton.addActionListener(new ActionListener() {
+           @Override
+            public void actionPerformed(ActionEvent e) {
+               String fortunaAleatoria = mostrarmensaje.AbrirGalleta();
+               mensaje.setText(fortunaAleatoria);
+           }
+        });
     }
 
 
